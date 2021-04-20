535da899668a071c910ca6a0a5eafb000144bf19
swapnil dahule <swapnildahule@swapnils-MacBook-Air.local>
2021-04-20 13:19:45 +0530

    test ui Modified to TestUI

--- TestUI/server.js
+++ TestUI/server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/testui'));
+app.use(express.static(__dirname + '/dist/TestUI'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/testui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/TestUI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
